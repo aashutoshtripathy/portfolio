0977aaf613f3dbc00b5de9c095a347a4b599c00b
Ashutosh <ashutoshkmr419@gmail.com>
2024-07-05 17:44:58 +0530

    modifes

--- frontend/src/components/specific/ChatList.js
+++ frontend/src/components/specific/ChatList.js
@@ -1,21 +1,45 @@
-import { Stack } from '@mui/material'
-import React from 'react'
+import { Stack } from "@mui/material";
+import React from "react";
+import ChatItem from "../shared/ChatItem";
 
-    const ChatList = ({w = "100%" ,chats=[],chatId,onlineUsers=[],newMessagesAlert=[{
-        chatId: "1",
-        count: 0,
-    }],handleDeleteChat}) => {
-      return (
-        <div>
-            <Stack width={w} direction={"column"}>
-                {
-                    chats?.map((item,index) => {
-                        return <div>{item}</div>
-                    })
-                }
-            </Stack>
-        </div>
-      )
-    }
-    
-    export default ChatList
+const ChatList = ({
+  w = "100%",
+  chats = [],
+  chatId,
+  onlineUsers = [],
+  newMessagesAlert = [
+    {
+      chatId: "1",
+      count: 0,
+    },
+  ],
+  handleDeleteChat,
+}) => {
+  return (
+    <div>
+      <Stack width={w} direction={"column"}>
+        {chats?.map((item, index) => {
+          const { name, _id, sameSender, members } = item;
+          const isOnline = members?.some((member) =>
+            onlineUsers.includes(member._id)
+          );
+          return (
+            <div>
+              <ChatItem
+                key={_id}
+                isOnline={isOnline}
+                name={name}
+                _id={_id}
+                index={index}
+                sameSender={chatId === _id}
+                handleDeleteChatOpen={handleDeleteChat}
+              />
+            </div>
+          );
+        })}
+      </Stack>
+    </div>
+  );
+};
+
+export default ChatList;
