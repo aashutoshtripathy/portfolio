582463cb24be7c518e058ddca31e044dc49ef939
Ashutosh <ashutoshkmr419@gmail.com>
2024-07-05 17:40:13 +0530

    added

---
+++ frontend/src/constants/SampleData.js
@@ -0,0 +1,13 @@
+export const sampleData = [{
+    name : "Ashutosh",
+    _id : "223",
+    sameSender : false,
+    isOnline : true,
+},
+{
+    name : "Ashutosh",
+    _id : "225",
+    sameSender : false,
+    isOnline : true,
+},
+]
